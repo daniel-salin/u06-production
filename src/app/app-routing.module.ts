e3c34037d43c4192a9815fe8cb8fd8f3879b1d16
daniel-salin <daniel.salin@chasacademy.se>
2019-02-07 10:48:46 +0100

    :sparkles: Set up home route and not found route
    -Home route connects to app.components.
    -Wildcard route connects to NotFoundComponent that contains a
     with message, link to home and basic styling.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:AppComponent },
+
+  {path: "**", component:NotFoundComponent, data: { title: "Page Not Found" } },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
