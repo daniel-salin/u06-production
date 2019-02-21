ec45cbd4616cef27551715af1f6dce1b2ad5029e
daniel-salin <daniel.salin@chasacademy.se>
2019-02-21 22:04:07 +0100

    :construction: Add before/after login services that regulate access to routes via tokens

--- src/app/lists/lists-routing.module.ts
+++ src/app/lists/lists-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ListsComponent } from './lists.component';
+import { AfterLoginService } from '../user/after-login.service';
 
 
 const listsRoutes: Routes = [
   
-  {path: "lists", component:ListsComponent},
+  {path: "lists", component:ListsComponent, canActivate: [AfterLoginService]},
 ];
 
     @NgModule({
--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./login.component";
 import { SignupComponent } from "./signup.component";
 import { UserComponent } from './user.component';
+import { BeforeLoginService } from './before-login.service';
+import { AfterLoginService } from './after-login.service';
 
 const userRoutes: Routes = [
-  { path: "login", component: LoginComponent },
-  { path: "register", component: SignupComponent },
-  { path: "user", component: UserComponent }
+  { path: "login", component: LoginComponent, canActivate: [BeforeLoginService] },
+  { path: "register", component: SignupComponent, canActivate: [AfterLoginService] },
+  { path: "user", component: UserComponent, canActivate: [AfterLoginService] }
 ];
 
 @NgModule({
